439589cdea1f0b08876cf556f22febc15881e477
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-14 12:22:10 -0300

    adding a new response

--- src/errors/reservate.response.ts
+++ src/errors/reservate.response.ts
@@ -1,10 +1,19 @@
 import {Field, ObjectType} from "type-graphql";
-import {Have} from "../entity/have.entity";
 
-//import {Place} from "../entity/place.entity";
+import {Reservate} from "../entity/reservate.entity";
+import {Have} from "../entity/have.entity";
 
 @ObjectType()
 class ReservateErrors {
+  @Field()
+  path: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class CreateErrors {
   @Field()
   path: string;
 
@@ -19,4 +28,13 @@ export class ReservateResponse {
 
   @Field(() => Have, {nullable: true})
   have?: Have;
+}
+
+@ObjectType()
+export class CreateReservationResponse {
+  @Field(() => [CreateErrors], {nullable: true})
+  errors?: CreateErrors[];
+
+  @Field(() => Reservate, {nullable: true})
+  reservate?: Reservate;
 }
